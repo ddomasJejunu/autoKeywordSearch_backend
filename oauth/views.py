c58a7a139c517267b5b44c80a62eb25351edc7c9
Jimen <hurjeesic@naver.com>
2020-01-24 07:15:31 +0900

    앱에 따른 기능 분류를 위한 불필요 코드 제거

--- oauth/views.py
+++ oauth/views.py
@@ -5,21 +5,6 @@ import requests
 import json
 
 # Create your views here.
-def kakaoRequestLogin(request):
-    client_id = "7347aa7b82ad90e1675c3065dc1ce2bf"
-    redirect_uri = "http://172.19.8.34:8000/oauth/kakao/login/"
-    app_uri = request.GET['appURI']
-    fail_app_uri = request.GET['failAppURI']
-
-    request.session['clientID'] = client_id
-    request.session['redirectURI'] = redirect_uri
-    request.session['appURI'] = app_uri
-    request.session['failAppURI'] = fail_app_uri
-
-    loginRequestURI = f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=talk_message";
-
-    return redirect(loginRequestURI)
-
 def getKakaotalkUserProfile(access_token):
     headers = {
         'Content-Type': "application/x-www-form-urlencoded;charset=utf-8",
@@ -102,61 +87,3 @@ def kakaoLogout(request):
     return JsonResponse({
         'success': False,
     }, json_dumps_params = { 'ensure_ascii': True})
-
-def send_to_me(request):
-    access_token = request.session.get('kakaoAccessToken')
-    template_dict_data = str({
-        "object_type": "feed",
-        "content": {
-            "title": "디저트 사진",
-            "description": "아메리카노, 빵, 케익",
-            "image_url": "http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
-            "image_width": 640,
-            "image_height": 640,
-            "link": {
-                "web_url": "http://www.daum.net",
-                "mobile_web_url": "http://m.daum.net",
-                "android_execution_params": "contentId=100",
-                "ios_execution_params": "contentId=100"
-            }
-        },
-        "social": {
-            "like_count": 100,
-            "comment_count": 200,
-            "shared_count": 300,
-            "view_count": 400,
-            "subscriber_count": 500
-        },
-        "buttons": [
-            {
-                "title": "웹으로 이동",
-                "link": {
-                    "web_url": "http://www.daum.net",
-                    "mobile_web_url": "http://m.daum.net"
-                }
-            },
-            {
-                "title": "앱으로 이동",
-                "link": {
-                    "android_execution_params": "contentId=100",
-                    "ios_execution_params": "contentId=100"
-                }
-            }
-        ]
-    })
-
-    kakao_to_me_uri = 'https://kapi.kakao.com/v2/api/talk/memo/default/send'
-
-    headers = {
-        'Content-Type': "application/x-www-form-urlencoded",
-        'Authorization': "Bearer " + access_token,
-    }
-
-    template_json_data = "template_object=" + str(json.dumps(template_dict_data))
-    template_json_data = template_json_data.replace("\"", "")
-    template_json_data = template_json_data.replace("'", "\"")
-    
-    response = requests.request(method="POST", url=kakao_to_me_uri, data=template_json_data, headers=headers)
-    print(response.json())
-
-    return redirect(str(request.session.get('appURI')))
