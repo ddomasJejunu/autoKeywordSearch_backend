b26f21d59295f1c9051e82a38511dc4b9b9eb90d
Jimen <hurjeesic@naver.com>
2020-01-24 07:14:11 +0900

    kakao login 방식 변경(get -> post)에 따른 세분화

--- oauth/urls.py
+++ oauth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('kakao/request/login/', views.kakaoRequestLogin, name='kakaoRequestLogin'),
+    path('kakao/login/code/', views.kakaoGetCode, name='kakaoGetCode'),
     path('kakao/login/', views.kakaoLogin, name='kakaoLogin'),
     path('kakao/logout/', views.kakaoLogout, name='kakaoLogout'),
 ]
--- oauth/views.py
+++ oauth/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import requests
 import json
 
@@ -30,56 +32,76 @@ def getKakaotalkUserProfile(access_token):
 
     return response
 
-def kakaoLogin(request):
-    code = request.GET['code']
-    # print('code = ' + str(code))
-
-    client_id = request.session.get('clientID')
-    redirect_uri = request.session.get('redirectURI')
-    app_uri = request.session.get('appURI')
-    fail_app_uri = request.session.get('failAppURI')
-    # print(f"client_id = {client_id}")
-    # print(f"redirect_uri = {redirect_uri}")
-    # print(f"app_uri = {app_uri}")
-    # print(f"fail_app_uri = {fail_app_uri}")
-
-    access_token_request_uri = f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
-    # print(access_token_request_uri)
-
-    access_token_request_uri_data = requests.get(access_token_request_uri)
-    json_data = access_token_request_uri_data.json()
-    # print(json_data)
-
+def kakaoGetCode(request):
     try:
-        access_token = json_data['access_token']
-    
-        request.session['kakaoAccessToken'] = access_token
-        # print(f"access token = {access_token}")
-
-        user_profile_info = getKakaotalkUserProfile(access_token)
-        user_id = user_profile_info['id']
-        nick_name = user_profile_info['kakao_account']['profile']['nickname']
+        code = request.GET['code']
         
-        print(f"app_uri = {app_uri}")
+        return JsonResponse({
+            'success': 'true',
+            'code': code,
+        }, json_dumps_params = { 'ensure_ascii': True })
+    except Exception:
+        return JsonResponse({
+            'success': False,
+        }, json_dumps_params = { 'ensure_ascii': True })
 
-        return redirect(f"{app_uri}?kakaoAccessToken={access_token}&userID={user_id}&nickName={nick_name}")
-    except KeyError:
-        print(f"fail_app_uri = {fail_app_uri}")
-        return redirect(f"{fail_app_uri}")
+@csrf_exempt
+def kakaoLogin(request):
+    if request.method == "POST":
+        client_id = request.POST['clientID']
+        redirect_uri = request.POST['redirectURI']
+        code = request.POST['code']
+        # print(f"client_id = {client_id}")
+        # print(f"redirect_uri = {redirect_uri}")
+        # print(f"code = {code}")
 
+        access_token_request_uri = f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
+        # print(access_token_request_uri)
+
+        access_token_request_uri_data = requests.get(access_token_request_uri)
+        json_data = access_token_request_uri_data.json()
+        # print(json_data)
+
+        try:
+            access_token = json_data['access_token']
+
+            user_profile_info = getKakaotalkUserProfile(access_token)
+            user_id = user_profile_info['id']
+            nick_name = user_profile_info['kakao_account']['profile']['nickname']
+            
+            return JsonResponse({
+                'success': True,
+                'kakaoAccessToken': access_token,
+                'userID': user_id,
+                'nickName': nick_name,
+            }, json_dumps_params = { 'ensure_ascii': True })
+        except KeyError:
+            pass
+
+    return JsonResponse({
+        'success': False,
+    }, json_dumps_params = { 'ensure_ascii': True })
+
+@csrf_exempt
 def kakaoLogout(request):
-    access_token = request.GET['kakaoAccessToken']
-    app_uri = request.GET['appURI']
+    if request.method == "POST":
+        access_token = request.POST['kakaoAccessToken']
 
-    headers = {
-        'Authorization': f"Bearer {access_token}",
-    }
+        headers = {
+            'Authorization': f"Bearer {access_token}",
+        }
 
-    kakao_logout_uri = 'https://kapi.kakao.com/v1/user/logout'
+        kakao_logout_uri = 'https://kapi.kakao.com/v1/user/logout'
 
-    resopnse = requests.get(kakao_logout_uri, headers=headers).json()
+        resopnse = requests.get(kakao_logout_uri, headers=headers).json()
 
-    return redirect(f"{app_uri}")
+        return JsonResponse({
+            'success': True,
+        }, json_dumps_params = { 'ensure_ascii': True })
+
+    return JsonResponse({
+        'success': False,
+    }, json_dumps_params = { 'ensure_ascii': True})
 
 def send_to_me(request):
     access_token = request.session.get('kakaoAccessToken')
